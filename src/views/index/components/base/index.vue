34560b60a18be588781bcbea362d761b2f33806f
skyujilong <skyujilong@163.com>
2020-07-22 15:12:22 +0800

    继承组件

---
+++ src/views/index/components/base/index.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import {
+  State,
+  Getter,
+  Action,
+  Mutation,
+  namespace
+} from 'vuex-class';
+@Component({})
+export default class Base extends Vue{
+    @Prop({}) data !: any;
+    @Prop({}) meta !: any;
+    @Getter('name') name !:string;
+}
+</script>
+
+<style>
+
+</style>
--- src/views/index/components/cmnt/index.vue
+++ src/views/index/components/cmnt/index.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Base from "../base/index.vue";
 import {
   State,
   Getter,
@@ -15,8 +16,14 @@ import {
 } from 'vuex-class';
 const cmntStore = namespace('cmnt');
 @Component({})
-export default class Cmnt extends Vue{
+export default class Cmnt extends Base{
     @cmntStore.State('text') text!:string;
+    mounted(){
+      // console.log(this.$props);
+      console.log(this.data);
+      console.log(this.meta);
+      console.log(this.name);
+    }
 }
 </script>
 <style>
